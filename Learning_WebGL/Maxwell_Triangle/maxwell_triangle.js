6aa73974ebf51c4010e1ef50dd206fedcac9e8b7
Krishna <coolkrishna31@gmail.com>
2015-07-22 10:30:49 +0530

    Add a new app - Maxwell Triangle.

---
+++ Learning_WebGL/Maxwell_Triangle/maxwell_triangle.js
@@ -0,0 +1,56 @@
+"use strict"
+
+/* global Triangle */
+/* global window */
+/* global document */
+/* global WebGLUtils */
+/* global Float32Array */
+/* global initShaders */
+
+var Maxwell_Triangle = function()
+{
+    var gl;
+    //var points;
+
+    var canvas = document.getElementById("gl-canvas");
+    var canvasWH = [canvas.width, canvas.height];
+    gl = WebGLUtils.setupWebGL(canvas);
+    if ( !gl ) 
+    {
+        alert("WebGL isn't available");
+    }
+    
+    var vertices = new Float32Array([-1, -1, 0, 1, 1, -1]);
+    
+    //Configure WebGL
+    gl.viewport(0,0, canvas.width, canvas.height);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    
+    //Load shaders and initialize attribute buffers
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+    
+    //Associate out shader variables with our data buffer
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray( vPosition);
+
+    gl.uniform2fv(gl.getUniformLocation(program, "canvasWH"), flatten(canvasWH));
+    
+    this.render = function()
+    {
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.drawArrays(gl.TRIANGLES, 0, 3);
+    };
+    
+    this.render();
+};
+
+window.onload = function init()
+{
+    new Maxwell_Triangle();
+};
+
