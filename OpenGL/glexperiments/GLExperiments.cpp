81e6e395045625ec4379a22ca0b6ba6a344eee09
Krishna Addepalli <coolkrishna31@gmail.com>
2017-08-07 16:48:37 +0530

    Made freeglut and glew cmake compatible.

--- OpenGL/glexperiments/GLExperiments.cpp
+++ OpenGL/glexperiments/GLExperiments.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include "glew.h"
 #include <GL/glut.h>
 #include <gl/GL.h>
 #include "TriangleRenderer.h"
@@ -30,6 +31,23 @@ void processSpecialKeys(int key, int x, int y)
 {
     pRenderer->processSpecialKeys(key, x, y);
 }
+
+void getGLInfo()
+{
+    auto err = glewInit();
+    if (GLEW_OK != err)
+    {
+        std::cout << std::endl << "Error initializing glew: " << glewGetErrorString(err) << std::endl;
+    }
+
+    std::cout << std::endl << "Using GLEW: " << glewGetString(GLEW_VERSION) << std::endl;
+
+    std::cout << "OpenGL 3.0 supported: " << std::boolalpha << static_cast<bool>(glewIsSupported("GL_VERSION_3_0"));
+
+    std::cout << "\nOpenGL 4.1 supported: " << std::boolalpha << static_cast<bool>(glewIsSupported("GL_VERSION_4_1"));
+
+}
+
 int main(int argc, char *argv[])
 {
     //pRenderer = std::make_unique<TriangleRenderer>();
@@ -42,6 +60,8 @@ int main(int argc, char *argv[])
     glutInitWindowSize(320, 320);
     glutCreateWindow("Lighthouse3D- GLUT Tutorial");
     
+    getGLInfo();
+
     glutDisplayFunc(renderScene); //Called when the Paint message is received from the OS
     glutReshapeFunc(windowResized); //Called when the window is resized;
     glutIdleFunc(renderScene); //Called when the application is idle.
